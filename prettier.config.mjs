872f6521f4e88103375641ceecd7eeaf79749694
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2025-09-15 12:02:34 -0300

    implementa issue 3

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * Prettier configuration for this project.
+ * Includes Tailwind CSS class sorting via prettier-plugin-tailwindcss.
+ */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+};
+
