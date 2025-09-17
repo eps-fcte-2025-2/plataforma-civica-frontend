0e0d96a70e57bd32497ae6d8d422ac14d570f2f5
Danilo Tertuliano <daniloctmelo@gmail.com>
2025-09-16 21:00:35 -0300

    Revert "feat: tailwind"
    This reverts commit 42eb870be6b07873c12fe876ac26dadb981dc7f9.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-
-import { ExampleButton } from "@/components/ExampleButton";
-import { ExampleSelect } from "@/components/ExampleSelect";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {/* Componentes de exemplo estilizados com Tailwind */}
-      <div className="flex flex-col items-center gap-8 mb-10 w-full max-w-md">
-        <ExampleButton />
-        <ExampleSelect />
-      </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
