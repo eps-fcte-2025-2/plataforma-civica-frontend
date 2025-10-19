afa4effc4b7588023c4a6650b120b21a4d0f3c5f
CaduRoriz <ceduardo.roriz@gmail.com>
2025-10-19 20:47:53 -0300

    adicao dos icones, alteracao das fontes, adequacao de cores, alteracao no header, footer e page principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+// layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import { Poppins } from "next/font/google";
+import { Poppins, Nunito, Inter, Roboto_Slab } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Plataforma Cívica",
@@ -14,13 +15,37 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+  variable: "--font-nunito",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-roboto-slab",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={poppins.variable}>
+    <html
+      lang="pt-BR"
+      className={`${poppins.variable} ${nunito.variable} ${inter.variable} ${robotoSlab.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
